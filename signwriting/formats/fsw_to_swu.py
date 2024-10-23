f2736f575f75f033fb8e443fe66055a359a2d2de
Amit Moryossef <amitmoryossef@gmail.com>
2024-10-23 22:12:19 +0200

    feat(fsw_to_swu): add conversion function

--- signwriting/formats/fsw_to_swu.py
+++ signwriting/formats/fsw_to_swu.py
@@ -1,3 +1,6 @@
+import re
+
+
 def key2swu(key: str) -> str:
     return chr(key2id(key) + 0x40000)
 
@@ -15,3 +18,31 @@ def symbol_line(symbol_id: int) -> str:
 
 def symbol_fill(symbol_id: int) -> str:
     return chr(symbol_id + 0x100000)
+
+
+def mark2swu(fsw_mark: str) -> str:
+    return {'A': '𝠀', 'B': '𝠁', 'L': '𝠂', 'M': '𝠃', 'R': '𝠄'}[fsw_mark]
+
+
+def num2swu(num: int) -> str:
+    return chr(0x1D80C + (num - 250))
+
+
+def position2swu(position: tuple) -> str:
+    return num2swu(position[0]) + num2swu(position[1])
+
+
+def fsw2swu(text: str) -> str:
+    text = text.replace("A", "𝠀").replace("B", "𝠁").replace("L", "𝠂").replace("M", "𝠃").replace("R", "𝠄")
+
+    # Convert positions
+    positions = re.findall(r'\d{3}x\d{3}', text)
+    for position in positions:
+        text = text.replace(position, position2swu(tuple(map(int, position.split("x")))))
+
+    # Convert symbols
+    symbols = re.findall(r'S.{5}', text)
+    for symbol in symbols:
+        text = text.replace(symbol, key2swu(symbol))
+
+    return text
--- signwriting/formats/test_fsw_to_sign.py
+++ signwriting/formats/test_fsw_to_sign.py
@@ -1,6 +1,7 @@
 import unittest
 
 from signwriting.formats.fsw_to_sign import fsw_to_sign
+from signwriting.formats.fsw_to_swu import fsw2swu
 
 
 class ParseSignCase(unittest.TestCase):
@@ -11,6 +12,10 @@ class ParseSignCase(unittest.TestCase):
         self.assertEqual(sign["box"]["symbol"], "M")
         self.assertEqual(sign["box"]["position"], (123, 456))
 
+    def test_conversion(self):
+        fsw = 'AS10011S10019S2e704S2e748M525x535S2e748483x510S10011501x466S2e704510x500S10019476x475'
+        swu = fsw2swu(fsw)
+        self.assertEqual(swu, '𝠀񀀒񀀚񋚥񋛩𝠃𝤟𝤩񋛩𝣵𝤐񀀒𝤇𝣤񋚥𝤐𝤆񀀚𝣮𝣭')
 
 if __name__ == '__main__':
     unittest.main()
